e22bce249d272e97c43685053af6741cbcd83a47
AlexeyBDev <jobabb82@yandex.ru>
2017-01-17 22:13:30 +0300

    + exercise_02_18

--- How_to_program_in_c++/exercise_02_21.cpp
+++ How_to_program_in_c++/exercise_02_21.cpp
@@ -1,26 +1,26 @@
 // exercise_02_21	070117
 
-#include <iostream>
-#include <iomanip>
-
-#define INTERVAL 8 // интервал между столбцами
-#define COUNTER 5  // количество выводимых строк
-
-using namespace std;
-
-
-int main(int argc, char* argv[])
-{
-	cout << setiosflags(ios::left) << setw(INTERVAL) << "N"
-		<< setw(INTERVAL) << "10*N"
-		<< setw(INTERVAL) << "100*N"
-		<< setw(INTERVAL) << "1000*N\n" << endl;
-	for(int coutnter = 1; coutnter <= COUNTER; coutnter++)
-	{
-		cout << setw(INTERVAL) << coutnter
-			<< setw(INTERVAL) << coutnter * 10
-			<< setw(INTERVAL) << coutnter * 100
-			<< setw(INTERVAL) << coutnter * 1000 << endl;
-	}
-	return 0;
-}
+//#include <iostream>
+//#include <iomanip>
+//
+//#define INTERVAL 8 // интервал между столбцами
+//#define COUNTER 5  // количество выводимых строк
+//
+//using namespace std;
+//
+//
+//int main(int argc, char* argv[])
+//{
+//	cout << setiosflags(ios::left) << setw(INTERVAL) << "N"
+//		<< setw(INTERVAL) << "10*N"
+//		<< setw(INTERVAL) << "100*N"
+//		<< setw(INTERVAL) << "1000*N\n" << endl;
+//	for(int coutnter = 1; coutnter <= COUNTER; coutnter++)
+//	{
+//		cout << setw(INTERVAL) << coutnter
+//			<< setw(INTERVAL) << coutnter * 10
+//			<< setw(INTERVAL) << coutnter * 100
+//			<< setw(INTERVAL) << coutnter * 1000 << endl;
+//	}
+//	return 0;
+//}
