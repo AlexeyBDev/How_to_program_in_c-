35ae016beeaf010eb970f5d79dce9a04a13080e0
AlexeyBDev <jobabb82@yandex.ru>
2017-01-18 23:48:08 +0300

    +exercise_02_22

--- How_to_program_in_c++/exercise_02_22.cpp
+++ How_to_program_in_c++/exercise_02_22.cpp
@@ -1,4 +1,17 @@
 // exercise_02_22	170117
+/*
+ * Псевдокод:
+ * ПОКА (Счетчик <= 10)
+ *		Введите Число
+ *		ЕСЛИ (Число == 1)
+ *			Большое_1 = Большое_2 = Число
+ *		ЕСЛИ (Число > Большого_1)
+ *			Большое_2 = Большое_1
+ *			Большое_1 = Число
+ *		ИНАЧЕ ЕСЛИ (Число > Большое_2)
+ *			Большое_2 = Число
+ *		++Счетчик	
+ */
 
 #include <iostream>
 #include <vector>
@@ -7,25 +20,42 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	int counter = 1,
+	int counter = 0,
 		number = 0,
 		largest = 0,
-		soLargest = 0;
+		veryLargest = 0;
 	vector<int> v(10); // массив для хранения 10 чисел
 	//cout << v[0] << endl;
-	while (counter <= 10)
+	while (counter < 10) // счетчик ввода 10 чисел
 	{
-		cout << "Enter the number(" << counter << "): ";
+		cout << "Enter the number(" << counter + 1 << "): "; // введите число
 		cin >> number;
-		if (counter == 1) soLargest = largest = number;
-		if (soLargest < number)
+		for (int i = counter; i >= 0; i--) // проверка массива на наличие введнного числа
 		{
-			largest = soLargest;
-			soLargest = number;
+			if(v[i] == number) // если введенное число уже было...
+			{
+				cout << "This number already!" << endl;
+				break; // то прерывание
+			}
+			if(i == 0) // если проверели все и дошли до нулевого элемента
+			{
+				v[counter] = number; // записываем число в массив
+				if (counter == 0) veryLargest = largest = number; // первое число равно нашим переменным
+				if (veryLargest < number) // если текущее число больше нашего самого большого
+				{
+					largest = veryLargest; // присваиваем числу поменьше значение большого
+					veryLargest = number; // большому числу присваиваем значение текущего
+				}
+				else if (largest < number) // сравнение числа поменьше с текущим числом
+				{
+					largest = number;
+				}
+				++counter; // счетчик
+			}
 		}
-		++counter;
+		
 	}
-	cout << "The so largest number is: " << soLargest << endl;
+	cout << "The so largest number is: " << veryLargest << endl;
 	cout << "The largest number is: " << largest << endl << endl;
 
 	return 0;
