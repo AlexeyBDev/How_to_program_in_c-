fcbeb20c1dda3fa3682f9c621b3b26eed0b5adb1
AlexeyBDev <jobabb82@yandex.ru>
2017-01-30 08:10:40 +0300

    add branch work_pc

--- How_to_program_in_c++/exercise_02_30.cpp
+++ How_to_program_in_c++/exercise_02_30.cpp
@@ -1,11 +1,11 @@
 // exercise_02_30	190117
 
-#include <iostream>
-
-using namespace std;
-
-int main(int argc, char* argv[])
-{
-
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(int argc, char* argv[])
+//{
+//
+//	return 0;
+//}
